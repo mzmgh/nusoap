e792d2834eabe971d0f5da90083eed466fa855d8
Mohammad Zare Moghadam <mzmoghadam72@gmail.com>
2017-05-14 20:22:27 +0430

    Fix array to string

--- lib/nusoap.php
+++ lib/nusoap.php
@@ -3122,7 +3122,11 @@ class soap_transport_http extends nusoap_base {
         	$err = 'cURL ERROR: '.curl_errno($this->ch).': '.$cErr.'<br>';
         	// TODO: there is a PHP bug that can cause this to SEGV for CURLINFO_CONTENT_TYPE
 			foreach(curl_getinfo($this->ch) as $k => $v){
-				$err .= "$k: $v<br>";
+				if (!is_scalar($v)) {
+                    $err .= "$k: ".print_r($v)."<br>";
+				} else {
+                    $err .= "$k: $v<br>";
+                }
 			}
 			$this->debug($err);
 			$this->setError($err);
